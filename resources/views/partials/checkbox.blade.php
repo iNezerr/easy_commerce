f20303d9b74d74164bdc23a461d80aeb8f6e155f
Eben <bonuzx1@gmail.com>
2024-02-29 12:54:13 +0000

    Finished cart page ui

---
+++ resources/views/partials/checkbox.blade.php
@@ -0,0 +1,12 @@
+<label class="check-container">Free shipping
+    <input type="radio" checked="checked" name="radio">
+    <span class="checkmark"></span>
+</label>
+<label class="check-container">Flat rate
+    <input type="radio" name="radio">
+    <span class="checkmark"></span>
+</label>
+<label class="check-container">Pickup
+    <input type="radio" name="radio">
+    <span class="checkmark"></span>
+</label>
---
+++ resources/views/partials/number.blade.php
@@ -0,0 +1,3 @@
+<div class="quantity">
+    <input type="number" min="1" step="1" value="1">
+</div>
